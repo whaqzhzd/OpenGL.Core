93f8d24bc901a697b5b080820cb0a83c4659c809
wang.hanbin <qq544430497@gmail.com>
2020-11-09 13:39:11 +0800

    add missing data

--- Defines/glVertexAttribType.cs
+++ Defines/glVertexAttribType.cs
@@ -26,87 +26,132 @@ using System;
 
 namespace OpenGL.Core
 {
-	/// <summary>
-	/// Data type for vertex attribs.
-	/// </summary>
-	public enum glVertexAttribType : uint
-	{
-		/// <summary>
-		/// Unsigned 8-bit integers.
-		/// </summary>
-		UNSIGNED_BYTE = 0x1401,
+    /// <summary>
+    /// Data type for vertex attribs.
+    /// </summary>
+    public enum glVertexAttribType : uint
+    {
+        /// <summary>
+        /// Unsigned 8-bit integers.
+        /// </summary>
+        UNSIGNED_BYTE = 0x1401,
 
-		/// <summary>
-		/// Signed 8-bit integers.
-		/// </summary>
-		BYTE = 0x1400,
+        /// <summary>
+        /// Signed 8-bit integers.
+        /// </summary>
+        BYTE = 0x1400,
 
-		/// <summary>
-		/// Unsigned 16-bit integers.
-		/// </summary>
-		UNSIGNED_SHORT = 0x1403,
+        /// <summary>
+        /// Unsigned 16-bit integers.
+        /// </summary>
+        UNSIGNED_SHORT = 0x1403,
 
-		/// <summary>
-		/// Signed 16-bit integers.
-		/// </summary>
-		SHORT = 0x1402,
+        /// <summary>
+        /// Signed 16-bit integers.
+        /// </summary>
+        SHORT = 0x1402,
 
-		/// <summary>
-		/// Unsigned 32-bit integers.
-		/// </summary>
-		UNSIGNED_INT = 0x1405,
+        /// <summary>
+        /// Unsigned 32-bit integers.
+        /// </summary>
+        UNSIGNED_INT = 0x1405,
 
-		/// <summary>
-		/// Signed 32-bit integers.
-		/// </summary>
-		INT = 0x1404,
+        /// <summary>
+        /// Signed 32-bit integers.
+        /// </summary>
+        INT = 0x1404,
 
-		/// <summary>
-		/// 16-bit floating-point values.
-		/// </summary>
-		HALF_FLOAT = 0x140B,
+        /// <summary>
+        /// 16-bit floating-point values.
+        /// </summary>
+        HALF_FLOAT = 0x140B,
 
-		/// <summary>
-		/// 32-bit floating-point values.
-		/// </summary>
-		/// 
-		FLOAT = 0x1406,
+        /// <summary>
+        /// 32-bit floating-point values.
+        /// </summary>
+        /// 
+        FLOAT = 0x1406,
 
-		/// <summary>
-		/// 64-bit floating-point values.
-		/// </summary>
-		DOUBLE = 0x140A,
+        /// <summary>
+        /// 64-bit floating-point values.
+        /// </summary>
+        DOUBLE = 0x140A,
 
-		/// <summary>
-		/// Corresponds to the GLSL type <c>fixed.</c>
-		/// </summary>
-		FIXED = 0x140C,
+        /// <summary>
+        /// Corresponds to the GLSL type <c>fixed.</c>
+        /// </summary>
+        FIXED = 0x140C,
 
-		/// <summary>
-		/// 3 * 10-bit + 2-bit as 32-bit unsigned integer in reversed order.
-		/// </summary>
-		UNSIGNED_INT_2_10_10_10_REV = 0x8368,
+        /// <summary>
+        /// 3 * 10-bit + 2-bit as 32-bit unsigned integer in reversed order.
+        /// </summary>
+        UNSIGNED_INT_2_10_10_10_REV = 0x8368,
 
-		/// <summary>
-		/// 11-bit + 11-bit + 10-bit floats as 32-bit unsigned integer in reversed order.
-		/// </summary>
-		UNSIGNED_INT_10F_11F_11F_REV = 0x8C3B,
+        /// <summary>
+        /// 11-bit + 11-bit + 10-bit floats as 32-bit unsigned integer in reversed order.
+        /// </summary>
+        UNSIGNED_INT_10F_11F_11F_REV = 0x8C3B,
 
-		/// <summary>
-		/// 3 * 10-bit + 2-bit as 32-bit unsigned integer in reversed order.
-		/// </summary>
-		INT_2_10_10_10_REV = 0x8D9F,
+        /// <summary>
+        /// 3 * 10-bit + 2-bit as 32-bit unsigned integer in reversed order.
+        /// </summary>
+        INT_2_10_10_10_REV = 0x8D9F,
 
-		///// <summary>
-		///// Unsigned 64-bit integer.
-		///// </summary>
-		//[Obsolete("OpenGL extension: GL_ARB_gpu_shader_int64")]
-		//UNSIGNED_INT64_ARB = 0x140F,
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT4 = 0x8B5C,
 
-		///// <summary>
-		///// Signed 64-bit integer.
-		///// </summary>
-		//[Obsolete("OpenGL extension: GL_ARB_gpu_shader_int64")]
-		//INT64_ARB = 0x140E,
-	}
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT4x2 = 0x8B69,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT4x3 = 0x8B6A,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT3 = 0x8B5B,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT3x2 = 0x8B67,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT3x4 = 0x8B68,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT2 = 0x8B5A,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT2x3 = 0x8B65,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        FLOAT_MAT2x4 = 0x8B66,
+
+        ///// <summary>
+        ///// Unsigned 64-bit integer.
+        ///// </summary>
+        //[Obsolete("OpenGL extension: GL_ARB_gpu_shader_int64")]
+        //UNSIGNED_INT64_ARB = 0x140F,
+
+        ///// <summary>
+        ///// Signed 64-bit integer.
+        ///// </summary>
+        //[Obsolete("OpenGL extension: GL_ARB_gpu_shader_int64")]
+        //INT64_ARB = 0x140E,
+    }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenGL.Core")]
 [assembly: AssemblyDescription("OpenGL binding for use in .NET-Framework based applications.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenGL.Core")]
 [assembly: AssemblyCopyright("Copyright © 2014-2016 by the Authors")]
 [assembly: AssemblyTrademark("OpenGL is a registered trademark, of Silicon Graphics International.")]
 [assembly: AssemblyCulture("")]
@@ -21,11 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6D7B914B-664C-41DF-80DC-A22756A94A2A")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("4.5.0.0")]
-[assembly: AssemblyFileVersion("4.5.0.0")]
